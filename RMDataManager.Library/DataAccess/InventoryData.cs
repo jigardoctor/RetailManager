fa813ccfb11596432ebc33e2464cf6f9350749de
JIGAR DOCTOR <JIGAR DOCTOR@JIGAR>
2022-02-27 17:04:03 +0530

    34

--- RMDataManager.Library/DataAccess/InventoryData.cs
+++ RMDataManager.Library/DataAccess/InventoryData.cs
@@ -1,4 +1,5 @@
-﻿using RMDataManager.Library.Internal.DataAccess;
+﻿using Microsoft.Extensions.Configuration;
+using RMDataManager.Library.Internal.DataAccess;
 using RMDataManager.Library.Models;
 using System;
 using System.Collections.Generic;
@@ -10,17 +11,22 @@ namespace RMDataManager.Library.DataAccess
 {
    public class InventoryData
     {
- 
+        private readonly IConfiguration _config;
+
+        public InventoryData(IConfiguration config)
+        {
+           _config = config;
+        }
         public List<InventoryModel> GetInventory()
         {
-            SqlDataAccess sql = new SqlDataAccess();
+            SqlDataAccess sql = new SqlDataAccess(_config);
             var output = sql.LoadData<InventoryModel, dynamic>("dbo.spInventory_GetAll", new { }, "RMData");
             return output;
         }
 
         public void SaveInventoryRecord(InventoryModel item)
         {
-            SqlDataAccess sql = new SqlDataAccess();
+            SqlDataAccess sql = new SqlDataAccess(_config);
             sql.SaveData<InventoryModel>("dbo.spInventory_Insert", item, "RMData");
         }
     }
